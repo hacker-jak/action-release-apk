8e9e7d790f9bc864dc934d741f9119773bc06d27
darkjakeco <darkjakeco@gmail.com>
2022-09-28 15:26:11 -0500

    don't use 'latest' tag...

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 hub checkout ${REPO_BRANCH}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" -m $(date +%Y%m%d%H%M%S) latest
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
